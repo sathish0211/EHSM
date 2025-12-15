672b84fadcd325ce9197cd84d629bcf99113e772
sathish0211 <sathishgp0212@gmail.com>
2025-12-15 16:17:53 +0530

    feat: Introduce Risk Assessment view and controller for displaying risk data.

--- webapp/controller/Risk.controller.js
+++ webapp/controller/Risk.controller.js
@@ -2,8 +2,10 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/UIComponent",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-], function (Controller, UIComponent, Filter, FilterOperator) {
+    "sap/ui/model/FilterOperator",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, UIComponent, Filter, FilterOperator, MessageToast, JSONModel) {
     "use strict";
 
     return Controller.extend("ehsm.controller.Risk", {
@@ -17,14 +19,54 @@ sap.ui.define([
         },
 
         _onRouteMatched: function () {
+            this._loadData();
+        },
+
+        _loadData: function () {
+            var oModel = this.getOwnerComponent().getModel();
             var oSessionModel = this.getOwnerComponent().getModel("session");
             var sEmpId = oSessionModel ? oSessionModel.getProperty("/EmployeeId") : "00000001";
 
             var aFilters = [new Filter("EmployeeId", FilterOperator.EQ, sEmpId)];
 
             var oTable = this.byId("riskTable");
-            var oBinding = oTable.getBinding("items");
-            oBinding.filter(aFilters);
+            oTable.setBusy(true);
+
+            var that = this;
+            oModel.read("/ZSG_EHSM_RISKSet", {
+                filters: aFilters,
+                success: function (oData) {
+                    oTable.setBusy(false);
+                    var oJsonModel = new JSONModel();
+                    oJsonModel.setData({ results: oData.results });
+                    that.getView().setModel(oJsonModel, "risks");
+
+                    if (!oTable.getBinding("items")) {
+                        oTable.bindItems({
+                            path: "risks>/results",
+                            template: new sap.m.ColumnListItem({
+                                cells: [
+                                    new sap.m.ObjectIdentifier({ title: "{risks>RiskId}" }),
+                                    new sap.m.Text({ text: "{risks>RiskDescription}" }),
+                                    new sap.m.Text({ text: "{risks>RiskCategory}" }),
+                                    new sap.m.ObjectStatus({ text: "{risks>RiskSeverity}", state: { path: "risks>RiskSeverity", formatter: function (sVal) { return sVal === 'High' ? 'Error' : sVal === 'Medium' ? 'Warning' : 'Success'; } } }),
+                                    new sap.m.Text({ text: "{risks>Likelihood}" }),
+                                    new sap.m.Text({ text: "{risks>MitigationMeasures}" }),
+                                    new sap.m.Text({ text: "{risks>Plant}" })
+                                ]
+                            })
+                        });
+                    }
+                },
+                error: function (oError) {
+                    oTable.setBusy(false);
+                    MessageToast.show("Failed to load risks");
+                }
+            });
+        },
+
+        onRefresh: function () {
+            this._loadData();
         },
 
         onNavBack: function () {
